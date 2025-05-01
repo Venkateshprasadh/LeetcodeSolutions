8e5d29d0de4f1a722f0ed57cd2115695266be466
venkateshprasadh <venkateshprasadh14@gmail.com>
2025-05-01 15:20:35 +0530

    optimised

--- src/main/java/TwoPointers/MoveZeros.java
+++ src/main/java/TwoPointers/MoveZeros.java
@@ -4,19 +4,31 @@ public class MoveZeros {
 
     private static int[] moveZeros(int arr[]) {
         int i = 0;
-        for (int j = 1; j < arr.length; j++) {
 
-            if ((arr[i] == 0) && arr[j] != 0) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
+        int nonZeroPtr = 0;
+        // Move all non-zero elements to the front
+        for (int num : arr) {
+            if (num != 0) {
+                arr[nonZeroPtr++] = num;
             }
-            if (arr[i] != 0) {
-                    i++;
-            }
-
         }
+        // Fill the remaining positions with zeros
+        while (nonZeroPtr < arr.length) {
+            arr[nonZeroPtr++] = 0;
+        }
+//        for (int j = 1; j < arr.length; j++) {
+//
+//            if ((arr[i] == 0) && arr[j] != 0) {
+//                int temp = arr[i];
+//                arr[i] = arr[j];
+//                arr[j] = temp;
+//                i++;
+//            }
+//            if (arr[i] != 0) {
+//                    i++;
+//            }
+//
+//        }
         return arr;
     }
 
