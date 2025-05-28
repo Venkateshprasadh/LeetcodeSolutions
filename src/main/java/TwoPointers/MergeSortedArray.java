ad97cf47da2a505bfdf4236c6ddd095208a9f202
venkateshprasadh <venkateshprasadh14@gmail.com>
2025-05-28 15:35:49 +0530

    first/brute

--- src/main/java/TwoPointers/MergeSortedArray.java
+++ src/main/java/TwoPointers/MergeSortedArray.java
@@ -1,4 +1,24 @@
 package TwoPointers;
 
+import java.util.Arrays;
+
 public class MergeSortedArray {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j=0;
+       for(int i=m;i<(m+n);i++)
+       {
+           nums1[i]=nums2[j];
+           j++;
+       }
+        Arrays.sort(nums1);
+        Arrays.stream(nums1).forEach(n1->System.out.println(n1));
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3,n=3;int[]  nums2 = {2,5,6};
+        merge(nums1,m,nums2,n);
+    }
 }
